488ff53929923afec55eb773085a95c02a5c22fa
katarzynaadamczyk <kasia.cholost@gmail.com>
2021-02-03 22:52:54 +0100

    repaired the bugs, working

--- main.py
+++ main.py
@@ -1,34 +1,31 @@
-from operator import itemgetter
-
 def sorter(item):
-    return(item[1], item[0])
+    return(-item[1], item[0])
 
 def rank(st, we, n):
     if len(st) == 0:
         return "No participants"
-    if n > len(st):
+    if n > len(we):
         return "Not enough participants"
 
     data = dict()
     j, p = 0, 0
 
-
     for i in range(len(we)):
         som = 0
-
         while j < len(st) and not st[j] == ',':
             som += ord(st[j].lower()) - ord("a") + 1
             j += 1
-
+        som += j - p
         som *= we[i]
-
         data[st[p:j]] = som
         j += 1
         p = j
 
     data_sorted = sorted(data.items(), key=sorter)
-    print(data_sorted)
-    return(data_sorted[-1 * n][0])
+    return(data_sorted[n-1][0])
 
-print("int('a'): ", ord('a'))
-print(rank("Addison,Jayden,Sofia,Michael,Andrew,Lily,Benjamin", [4, 2, 1, 4, 3, 1, 2], 4))
+def main():
+    print(rank("Addison,Jayden,Sofia,Michael,Andrew,Lily,Benjamin", [4, 2, 1, 4, 3, 1, 2], 4))
+
+if __name__ == "__main__":
+    main()
